17ac8ee0694afb7026e9fd8e5ef5fdeaa979cad8
VISTALL <vistall.valeriy@gmail.com>
2013-05-29 16:59:39 +0300

    initial code from https://bitbucket.org/atlassian/jetbrains-bitbucket-connector

---
+++ src/org/bitbucket/connectors/jetbrains/BitbucketCheckoutProvider.java
@@ -0,0 +1,154 @@
+package org.bitbucket.connectors.jetbrains;
+
+import com.intellij.ide.GeneralSettings;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vcs.CheckoutProvider;
+import com.intellij.openapi.vcs.VcsKey;
+import com.intellij.util.SystemProperties;
+import org.apache.commons.httpclient.URIException;
+import org.bitbucket.connectors.jetbrains.ui.BitbucketBundle;
+import org.bitbucket.connectors.jetbrains.ui.BitbucketCloneProjectDialog;
+import org.bitbucket.connectors.jetbrains.vcs.GitHandler;
+import org.bitbucket.connectors.jetbrains.vcs.HgHandler;
+import org.bitbucket.connectors.jetbrains.vcs.VcsHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BitbucketCheckoutProvider implements CheckoutProvider {
+    public void doCheckout(@NotNull Project project, @Nullable Listener listener) {
+        List<RepositoryInfo> availableRepos = BitbucketUtil.getRepositories(project, false);
+        if (availableRepos == null) {
+            return;
+        }
+
+        if (availableRepos.isEmpty()) {
+            Messages.showErrorDialog(project, BitbucketBundle.message("clone-err"), BitbucketBundle.message("cannot-clone"));
+            return;
+        }
+        Collections.sort(availableRepos, new Comparator<RepositoryInfo>() {
+            public int compare(final RepositoryInfo r1, final RepositoryInfo r2) {
+                int res = r1.getOwner().compareTo(r2.getOwner());
+                if (res != 0) {
+                    return res;
+                }
+                return r1.getName().compareTo(r2.getName());
+            }
+        });
+
+        String clonePath;
+        String lastProjectLocation = GeneralSettings.getInstance().getLastProjectLocation();
+        String userHome = SystemProperties.getUserHome();
+        if (lastProjectLocation != null) {
+            clonePath = lastProjectLocation.replace('/', File.separatorChar);
+        } else {
+            clonePath = userHome.replace('/', File.separatorChar) + File.separator +
+                    ApplicationNamesInfo.getInstance().getLowercaseProductName() + "Projects";
+        }
+        File file = new File(clonePath);
+        if (!file.exists() || !file.isDirectory()) {
+            clonePath = userHome;
+        }
+
+        BitbucketCloneProjectDialog checkoutDialog = new BitbucketCloneProjectDialog(project, availableRepos);
+        checkoutDialog.setSelectedPath(clonePath);
+        checkoutDialog.show();
+        if (!checkoutDialog.isOK()) {
+            return;
+        }
+
+        File folder = new File(checkoutDialog.getSelectedPath());
+        if (!folder.exists() || !folder.isDirectory()) {
+            return;
+        }
+
+        String projectName = checkoutDialog.getProjectName();
+        if (!StringUtil.isEmpty(projectName)) {
+            folder = new File(folder, projectName);
+            if (!folder.exists()) {
+                if (!folder.mkdir()) {
+                    return;
+                }
+            } else if (!folder.isDirectory()) {
+                return;
+            }
+        }
+
+        RepositoryInfo repository = checkoutDialog.getSelectedRepository();
+
+        try {
+            String repositoryUrl;
+            boolean git;
+            if (repository != null) {
+                repositoryUrl = repository.getCheckoutUrl();
+                git = repository.isGit();
+            } else {
+                repositoryUrl = checkoutDialog.getRepositoryUrl();
+                if (BitbucketUtil.isHttpUrl(repositoryUrl)) {
+                    repositoryUrl = RepositoryInfo.addPassword(repositoryUrl, false);
+                }
+                git = GitHandler.isGitRepository(repositoryUrl);
+            }
+            checkout(project, repositoryUrl, folder.getPath(), git, listener);
+        } catch (URIException e) {
+            Messages.showErrorDialog(project, e.getMessage(), BitbucketBundle.message("url-encode-err"));
+        }
+    }
+
+    private void checkout(final Project project, final String repositoryUrl, final String folder, final boolean git, final Listener listener) {
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+            public void run() {
+                FileDocumentManager.getInstance().saveAllDocuments();
+            }
+        });
+
+        new Task.Backgroundable(project, BitbucketBundle.message("checkouting"), true) {
+            public void run(@NotNull ProgressIndicator progressIndicator) {
+                final VcsHandler vcsHandler = git ? new GitHandler() : new HgHandler();
+                if (vcsHandler.checkout(project, folder, repositoryUrl)) {
+                    ApplicationManager.getApplication().invokeLater(new Runnable() {
+                        public void run() {
+                            if (listener != null) {
+                                directoryCheckedOut(listener, new File(folder), vcsHandler.getVcs(project).getKeyInstanceMethod());
+                                listener.checkoutCompleted();
+                            }
+                        }
+                    });
+                }
+            }
+        }.queue();
+    }
+
+    private void directoryCheckedOut(Listener listener, File folder, VcsKey vcsKey) {
+        for (Method m: listener.getClass().getDeclaredMethods()) {
+            if ("directoryCheckedOut".equals(m.getName())) {
+                Object[] params = m.getParameterTypes().length == 2 ?
+                        new Object[] { folder, vcsKey } : // new API
+                        new Object[] { folder }; // old API
+                try {
+                    m.invoke(listener, params);
+                    return;
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public String getVcsName() {
+        return BitbucketUtil.BITBUCKET;
+    }
+}
---
+++ src/org/bitbucket/connectors/jetbrains/vcs/GitHandler.java
@@ -0,0 +1,103 @@
+package org.bitbucket.connectors.jetbrains.vcs;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vfs.VirtualFile;
+import git4idea.GitUtil;
+import git4idea.GitVcs;
+import git4idea.commands.GitCommand;
+import git4idea.commands.GitHandlerUtil;
+import git4idea.commands.GitLineHandler;
+import git4idea.commands.GitSimpleHandler;
+import org.bitbucket.connectors.jetbrains.BitbucketUtil;
+import org.bitbucket.connectors.jetbrains.ui.BitbucketBundle;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * User: leha2000
+ * Date: Oct 14, 2011
+ * Time: 11:35:12 AM
+ */
+public class GitHandler implements VcsHandler {
+
+    public boolean checkout(Project project, String folderPath, String repositoryUrl) {
+        File folder = new File(folderPath);
+        GitSimpleHandler handler = new GitSimpleHandler(project, folder.getParentFile(), GitCommand.CLONE);
+        handler.addParameters(repositoryUrl, folder.getName());
+        handler.runInCurrentThread(null);
+        return true;
+    }
+
+    public static boolean isGitRepository(String url) {
+        return url != null && url.endsWith(".git");
+    }
+
+    public boolean push(Project project, VirtualFile root, String repositoryUrl) {
+        if (BitbucketUtil.isHttpUrl(repositoryUrl)) {
+            if (!disableHttpSslCheck(project, root)) {
+                return false;
+            }
+        }
+
+        GitLineHandler handler = new GitLineHandler(project, root, GitCommand.PUSH);
+        handler.addParameters("origin", "master");
+        Collection<VcsException> err = GitHandlerUtil.doSynchronouslyWithExceptions(handler);
+        return err.isEmpty();
+    }
+
+    private boolean disableHttpSslCheck(Project project, VirtualFile root) {
+        return execute(project, root, GitCommand.CONFIG, "--global", "http.sslverify", "false");
+    }
+
+    public void setRepositoryDefaultUrl(Project project, VirtualFile root, String repositoryUrl) throws IOException {
+        execute(project, root, GitCommand.REMOTE, "rm", "origin");
+        execute(project, root, GitCommand.REMOTE, "add", "origin", repositoryUrl);
+    }
+
+    public VirtualFile getRepositoryRoot(VirtualFile folder) {
+        return folder != null ? GitUtil.getGitRootOrNull(new File(folder.getPath())) : null;
+    }
+
+    public boolean initRepository(Project project, VirtualFile root) {
+        if (!execute(project, root, GitCommand.INIT)) {
+            return false;
+        }
+        GitLineHandler h = new GitLineHandler(project, root, GitCommand.ADD);
+        h.addParameters(".");
+        h.setNoSSH(true);
+        GitHandlerUtil.doSynchronously(h, BitbucketBundle.message("create-local-repository"), BitbucketBundle.message("share-project-on-bitbucket"));
+        if (!h.errors().isEmpty()) {
+            return false;
+        }
+
+        h = new GitLineHandler(project, root, GitCommand.COMMIT);
+        h.addParameters("-m", BitbucketBundle.message("initial-rev-msg"));
+        h.setNoSSH(true);
+        GitHandlerUtil.doSynchronously(h, BitbucketBundle.message("create-local-repository"), BitbucketBundle.message("share-project-on-bitbucket"));
+        return h.errors().isEmpty();
+    }
+
+    public AbstractVcs getVcs(Project project) {
+        return GitVcs.getInstance(project);
+    }
+
+    private static boolean execute(Project project, VirtualFile root, GitCommand cmd, String... params) {
+        GitSimpleHandler handler = new GitSimpleHandler(project, root, cmd);
+        handler.addParameters(params);
+        handler.setSilent(true);
+        handler.setNoSSH(true);
+        try {
+            handler.run();
+            if (handler.getExitCode() != 0) {
+                return false;
+            }
+        } catch (VcsException e) {
+            return false;
+        }
+        return true;
+    }
+}
